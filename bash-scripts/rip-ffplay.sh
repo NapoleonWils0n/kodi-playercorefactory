e4ba61deebf0e59cbb36bb0f9a074752dd316c3e
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-09-17 21:01:02 +0100

    ffplay m3u8 user agent regex

--- bash-scripts/rip-ffplay.sh
+++ bash-scripts/rip-ffplay.sh
@@ -24,6 +24,9 @@ XFORWARDIP=`echo "$XFORWARD" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-
 USEREF=`echo "$VIDEOURL" | grep -Eo '(http|https)://[a-zA-Z0-9:0-9./?=_-]*\.(mkv|mp4|m3u8|avi|flv)\|User-Agent=[a-zA-Z]*/[0-9]{1,1}\.[0-9]{1,1}(.*)\&Referer=(http|https)://[a-zA-Z0-9./?=_-]*(/|\.html)'`
 USERAGENT=`echo "$USEREF" | grep -Eo 'User-Agent=[a-zA-Z]*/[0-9]{1,1}\.[0-9]{1,1}(.[^&]*)'`
 REFERER=`echo "$USEREF" | grep -Eo 'Referer=(http|https)://[a-zA-Z0-9./?=_-]*(/|\.html)' | sed 's/Referer=//'`
+M3U8USERAGENT=`echo "$VIDEOURL" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_@%-]*\.(m3u8)\|user-agent=[a-zA-Z]*'`
+M3U8UAG=`echo "$M3U8USERAGENT" | grep -Eo 'user-agent=[a-zA-Z]*' | sed 's/user-agent=//'`
+M3U8UAGM3U8=`echo "$M3U8USERAGENT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_@%-]*\.(m3u8)'`
 
 case "$url" in
 	"$VIDEOFILE")
@@ -37,6 +40,9 @@ case "$url" in
 	;;
 	"$USEREF")
 	ffplay -fs -user-agent "$USERAGENT" -headers 'Referer: '"$REFERER"''$'\r\n' "$VIDEOFILE"
+	;;
+	"$M3U8USERAGENT")
+	ffplay -hide_banner -loglevel error -fs -user-agent "$M3U8UAG" "$M3U8UAGM3U8"
 	;;
 	*)
 	ffplay -hide_banner -loglevel error -fs "$VIDEOURL"
