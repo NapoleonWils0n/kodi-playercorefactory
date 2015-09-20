80fcbfd01f3acba6aea20be7683dbafebe911ba6
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-09-20 02:56:31 +0100

    rip steam now accepts duration

--- bash-scripts/rip-stream.sh
+++ bash-scripts/rip-stream.sh
@@ -6,13 +6,15 @@ if [[ $# < 1 ]]
 	then
 		printf "%b" "error not enough aruments passed to the script" '\n'
 		exit 1 # exit with non zero		
-elif [[ $# > 1 ]]
+elif [[ $# > 3 ]]
 	 then
 		printf "%b" "too many aruments passed to the script" '\n'
 		exit 2 # exit with non zero
 else
 
 VIDEOURL=`cat "$1"`
+TFLAG="$2"
+DURATION="$3"
 
 echo $VIDEOURL |
 while read url
@@ -25,6 +27,7 @@ case "$url" in
 	-c:v libx265 -preset ultrafast -tune zero-latency \
 	-x265-params crf=28 \
 	-c:a aac -strict experimental -b:a 192k \
+	${TFLAG} ${DURATION} \
 	-loglevel error \
 	-maxrate 4000k -bufsize 4000k \
 	-f mpegts udp://239.253.253.4:1234?pkt_size=1316
@@ -32,12 +35,14 @@ case "$url" in
 "$M3U8")
 	ffmpeg -hide_banner -i "$M3U8" \
 	-c:v copy -c:a copy \
+	${TFLAG} ${DURATION} \
 	-loglevel error \
 	-f mpegts udp://239.253.253.4:1234?pkt_size=1316
 	;;
 "$XFORWARD")
 	ffmpeg -hide_banner -headers 'X-Forwarded-For: '"$XFORWARDIP"''$'\r\n' -i "$M3U8" \
 	-c:v copy -c:a copy \
+	${TFLAG} ${DURATION} \
 	-loglevel error \
 	-f mpegts udp://239.253.253.4:1234?pkt_size=1316
 	;;
@@ -46,6 +51,7 @@ case "$url" in
 	-c:v libx265 -preset ultrafast -tune zero-latency \
 	-x265-params crf=28 \
 	-c:a aac -strict experimental -b:a 192k \
+	${TFLAG} ${DURATION} \
 	-loglevel error \
 	-maxrate 4000k -bufsize 4000k \
 	-f mpegts udp://239.253.253.4:1234?pkt_size=1316
@@ -53,6 +59,7 @@ case "$url" in
 "$M3U8USERAGENT")
 	ffmpeg -hide_banner -loglevel error -user-agent "$M3U8UAG" -i "$M3U8UAGM3U8" \
 	-c:v copy -c:a copy \
+	${TFLAG} ${DURATION} \
 	-loglevel error \
 	-f mpegts udp://239.253.253.4:1234?pkt_size=1316
 	;;
@@ -61,6 +68,7 @@ case "$url" in
 	-c:v libx265 -preset ultrafast -tune zero-latency \
 	-x265-params crf=28 \
 	-c:a aac -strict experimental -b:a 192k \
+	${TFLAG} ${DURATION} \
 	-loglevel error \
 	-maxrate 4000k -bufsize 4000k \
 	-f mpegts udp://239.253.253.4:1234?pkt_size=1316
