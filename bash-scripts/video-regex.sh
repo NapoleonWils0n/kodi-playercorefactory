9eb0f265ae3251c2280327ae789fffc663809df7
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-10-05 01:16:25 +0100

    m3u8 user agent referer regex

--- bash-scripts/video-regex.sh
+++ bash-scripts/video-regex.sh
@@ -10,3 +10,7 @@ REFERER=`echo "$USEREF" | grep -Eo 'Referer=(http|https)://[a-zA-Z0-9./?=_-]*(/|
 M3U8USERAGENT=`echo "$VIDEOURL" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_@%-]*\.(m3u8)\|user-agent=[a-zA-Z]*'`
 M3U8UAG=`echo "$M3U8USERAGENT" | grep -Eo 'user-agent=[a-zA-Z]*' | sed 's/user-agent=//'`
 M3U8UAGM3U8=`echo "$M3U8USERAGENT" | grep -Eo '(http|https)://[a-zA-Z0-9:0-9./?=_@%-]*\.(m3u8)'`
+
+M3U8UAGENTREFERER=`echo "$VIDEOURL" | grep -Eo '(^http|https)://[a-zA-Z0-9:0-9./?=_&@%|()[:blank:],;-]*'`
+M3U8UAGNTREF=`echo "$M3U8UAGENTREFERER" | grep -Eo '(^http|https)://[a-zA-Z0-9:0-9./?=_&@%]*'`
+M3U8USERAGENTREF=`echo "$M3U8UAGENTREFERER" | grep -Eo 'User-Agent=[a-zA-Z0-9/.()[:blank:],;-]*'`
