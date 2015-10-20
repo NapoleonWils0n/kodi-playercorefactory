35af4cae56ad1d5cbbe71ccba406ccde0e4923f8
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-10-20 16:51:13 +0100

    windows ffmpeg useragent referer script

---
+++ windows-batch-scripts/ffmpeg-useragent-referer.cmd
@@ -0,0 +1,10 @@
+setlocal EnableDelayedExpansion
+Set LF=^
+
+
+for /f %%a in ('copy /z "%~f0" nul') do set "CR=%%a"
+set VIDEOURL=http://example.com/.m3u8
+set USERAGENT=Mozilla/5.0
+set REFERER=example.com
+Set VNAME=video.mkv
+c:\ffmpeg\bin\ffplay.exe -user_agent "%USERAGENT%" -headers "Referer: %REFERER%!CR!!LF!" -i "%VIDEOURL%" -c:v copy -c:a copy "%VNAME%"
