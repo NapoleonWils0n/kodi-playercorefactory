bd99b260042b85d8ef88de6770f3e94b50cb1c7d
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-10-20 23:01:50 +0100

    ffplay full screen

--- windows-batch-scripts/ffplay/ffplay-useragent.cmd
+++ windows-batch-scripts/ffplay/ffplay-useragent.cmd
@@ -5,4 +5,4 @@ Set LF=^
 for /f %%a in ('copy /z "%~f0" nul') do set "CR=%%a"
 set VIDEOURL=http://example.com/video.mkv
 set USERAGENT=Mozilla/5.0
-c:\ffmpeg\bin\ffplay.exe -hide_banner -user_agent "%USERAGENT%" -i "%VIDEOURL%"
+c:\ffmpeg\bin\ffplay.exe -hide_banner -user_agent "%USERAGENT%" -fs -i "%VIDEOURL%"
--- windows-batch-scripts/ffplay/ffplay-xfoward-m3u8.cmd
+++ windows-batch-scripts/ffplay/ffplay-xfoward-m3u8.cmd
@@ -5,4 +5,4 @@ Set LF=^
 for /f %%a in ('copy /z "%~f0" nul') do set "CR=%%a"
 set VIDEOURL=http://example.com/video.m3u8
 Set XFORWARDIP=123.45.67.89
-c:\ffmpeg\bin\ffplay.exe -hide_banner -headers "X-Forwarded-For: %XFORWARDIP%!CR!!LF!" -i "%VIDEOURL%"
+c:\ffmpeg\bin\ffplay.exe -hide_banner -headers "X-Forwarded-For: %XFORWARDIP%!CR!!LF!" -fs -i "%VIDEOURL%"
