abf43b64f7cbd468fb34345150b6b0f813311ebd
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-10-20 21:02:10 +0100

    ffmpeg

--- windows-batch-scripts/ffmpeg/ffmpeg-m3u8-useragent.cmd
+++ windows-batch-scripts/ffmpeg/ffmpeg-m3u8-useragent.cmd
@@ -6,4 +6,4 @@ for /f %%a in ('copy /z "%~f0" nul') do set "CR=%%a"
 set VIDEOURL=http://example.com/video.m3u8
 set USERAGENT=Mozilla/5.0
 Set VNAME=video.mkv
-c:\ffmpeg\bin\ffplay.exe -user_agent "%USERAGENT%" -i "%VIDEOURL%" -c:v copy -bsf:a aac_adtstoasc "%VNAME%"
+c:\ffmpeg\bin\ffmpeg.exe -user_agent "%USERAGENT%" -i "%VIDEOURL%" -c:v copy -bsf:a aac_adtstoasc "%VNAME%"
--- windows-batch-scripts/ffmpeg/ffmpeg-m3u8.cmd
+++ windows-batch-scripts/ffmpeg/ffmpeg-m3u8.cmd
@@ -5,4 +5,4 @@ Set LF=^
 for /f %%a in ('copy /z "%~f0" nul') do set "CR=%%a"
 set VIDEOURL=http://example.com/video.m3u8
 Set VNAME=video.mkv
-c:\ffmpeg\bin\ffplay.exe -i "%VIDEOURL%" -c:v copy -bsf:a aac_adtstoasc "%VNAME%"
+c:\ffmpeg\bin\ffmpeg.exe -i "%VIDEOURL%" -c:v copy -bsf:a aac_adtstoasc "%VNAME%"
