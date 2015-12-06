fd3a20bb0d5f6b730e7b5eb7d879e9a1e8eabe6f
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-12-06 22:22:31 +0000

    iplayer regex

--- bash-scripts-windows/video-regex.sh
+++ bash-scripts-windows/video-regex.sh
@@ -13,3 +13,4 @@ M3U8USERAGENT=`echo "$VIDEOURL" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_@%-]*\
 M3U8UAGENTREFERER=`echo "$VIDEOURL" | grep -Eo '(^http|https)://[a-zA-Z0-9:0-9./?=_&@%|()[:blank:],;-]*'`
 M3U8UAGNTREF=`echo "$M3U8UAGENTREFERER" | grep -Eo '(^http|https)://[a-zA-Z0-9:0-9./?=_&@%-]*'`
 REFUSERAGENT=`echo "$VIDEOURL" | grep -Eo '(http|https)://[a-zA-Z0-9:0-9./?=_-]*\|Referer=(http|https)(://|%3A%2F%2F)[a-zA-Z%0-9./?=_-]*\&User-Agent=[a-zA-Z0-9/.()[:blank:],:;%+-]*'`
+BEEB=`echo "$VIDEOURL" | grep -Eo '^(http|https|rtmp)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]_=-]*'`
