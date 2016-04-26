bbc6ff2e0e7559be1263a20740b6a2aa6461531d
Daniel J Wilcox <danieljwilcox@gmail.com>
2016-04-26 17:00:55 +0100

    rtmpdump

--- bash-scripts/video-regex.sh
+++ bash-scripts/video-regex.sh
@@ -17,6 +17,7 @@ REFUSERAGENT=`echo "$VIDEOURL" | grep -Eo '(http|https)://[a-zA-Z0-9:0-9./?=_-]*
 BEEB=`echo "$VIDEOURL" | grep -Eo '^(http|https)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]_=-]*'`
 GVID=`echo "$VIDEOURL" | grep -Eo '^(http|https)://[a-zA-Z0-9:0-9/.&;,~*?_=-]*'`
 #RTMP=`echo "$VIDEOURL" | grep -Eo '^(rtmp|rtmpe)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]_=-]*'`
-#RTMP=`echo "$VIDEOURL" | grep -Eo '^(rtmp|rtmpe)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]_=-]*'`
-RTMP=`echo "$VIDEOURL" | grep -Eo '^(rtmp|rtmpe)://[a-zA-Z0-9\:0-9/.&;,~*?[:blank:]#%@!()_=-]*'`
-RTMP2=`echo "$VIDEOURL" | grep -Eo '[a-zA-Z0-9#%\.\?\(\)@!]*'`
+#RTMP=`echo "$VIDEOURL" | grep -Eo '^(rtmp|rtmpe)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]#%@!()_=-]*'`
+RTMP=`echo "$VIDEOURL" | grep -Eo '^(rtmp|rtmpe)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]#%@!()_=-]*'`
+#RTMP=`echo "$VIDEOURL" | grep -Eo '^(rtmp|rtmpe)://[a-zA-Z0-9:0-9/.&;,~*?[:blank:]#%@!()_=-]*[a-zA-Z0-9,\]*'`
+#RTMP2=`echo "$VIDEOURL" | grep -Eo '[a-zA-Z0-9#%\.\?\(\)@!]*'`
